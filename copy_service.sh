04396f29ae2114165e7baec53ef79c6e8cbe4168
Ubuntu <ubuntu@ip-172-31-20-109.ap-southeast-1.compute.internal>
2021-05-16 10:55:51 +0800

    clean up wsgi stuff and myflask.conf

--- copy_service.sh
+++
@@ -1,3 +0,0 @@
-sudo cp myflask.service /etc/systemd/system/myflask.service
-
-sudo cp myflask.conf /etc/nginx/sites-available/myflask.conf
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from myflask import app
-
-if __name__ == "__main__":
-    app.run()
